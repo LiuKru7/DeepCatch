74cefbec9d2c7d2194ca822f392e7d94e0c64069
Mindaugas Kašėta <Mindaugas.kas@gmail.com>
2025-05-09 00:35:24 +0300

    Aquatic controleris. servisas ir exceptionas.

--- src/main/java/finalProject/fishingLogTracker/fishingTracker/controller/AquaticController.java
+++ src/main/java/finalProject/fishingLogTracker/fishingTracker/controller/AquaticController.java
@@ -4,6 +4,7 @@ import finalProject.fishingLogTracker.fishingTracker.dto.AquaticRequest;
 import finalProject.fishingLogTracker.fishingTracker.dto.AquaticResponse;
 import finalProject.fishingLogTracker.fishingTracker.service.AquaticService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("api/aquatic")
 @RequiredArgsConstructor
+@Slf4j
 public class AquaticController {
 
     private final AquaticService aquaticService;
@@ -28,6 +30,21 @@ public class AquaticController {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(aquaticService.addNewAquatic(aquaticRequest));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<AquaticResponse> updateAquatic (
+            @PathVariable Long id,
+            @RequestBody AquaticRequest aquaticRequest) {
+        log.info("Received request to update Aquatic ID: {}", id);
+        return ResponseEntity.ok(aquaticService.updateAquatic(id, aquaticRequest));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteAquatic(@PathVariable Long id) {
+        log.info("Received request to delete Aquatic: {}", id);
+        aquaticService.deleteAquatic(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
--- src/main/java/finalProject/fishingLogTracker/fishingTracker/repository/CatchRepository.java
+++ src/main/java/finalProject/fishingLogTracker/fishingTracker/repository/CatchRepository.java
@@ -1,6 +1,5 @@
 package finalProject.fishingLogTracker.fishingTracker.repository;
 
-import finalProject.fishingLogTracker.fishingTracker.entity.Bait;
 import finalProject.fishingLogTracker.fishingTracker.entity.Catch;
 import finalProject.fishingLogTracker.fishingTracker.enums.BaitType;
 import finalProject.fishingLogTracker.fishingTracker.enums.FishingStyle;
@@ -8,7 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface CatchRepository extends JpaRepository<Catch, Long> {
